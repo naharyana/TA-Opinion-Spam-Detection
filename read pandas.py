db1e015532a57416dcb0829c987770d54d8ccf2f
Ana Alimatus Zaqiyah <naharyana57@gmail.com>
2019-02-26 23:38:52 +0700

    read df

--- read pandas.py
+++ read pandas.py
@@ -7,6 +7,12 @@ Created on Tue Feb 26 09:42:30 2019
 
 import pandas as pd
 
-df = pd.read_csv('productInfoXML-reviewed-mProductssv.csv', sep=',', encoding='latin-1')
+#df = pd.read_csv('productInfoXML-reviewed-mProductssv.csv', sep=',', encoding='latin-1')
+df1 = pd.read_csv('labeledSpam.csv', sep=',', encoding='latin-1')
 
-for 
+jumlah_data = len(df)
+jumlah_data_spam = len (df1)
+
+#df2 = df1[0:3] #kalau mau akses data ke 0 sampe 2 
+#for in range jumlah_data:
+    
