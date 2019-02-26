04ad8d9f50c445a71f60e43066668b0c5f0a276e
Ana Alimatus Zaqiyah <naharyana57@gmail.com>
2019-02-26 11:20:49 +0700

    read csv to dataframe

---
+++ read pandas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 09:42:30 2019
+
+@author: hp
+"""
+
+import pandas as pd
+
+df = pd.read_csv('productInfoXML-reviewed-mProductssv.csv', sep=',', encoding='latin-1')
+
+for 
